91d294e0738d929c5eaee1a689a02cb26b098471
Aurimas <aurimas1rimkus@gmail.com>
2017-11-16 22:02:33 +0200

    Add some code for testing

--- src/WeatherBundle/Controller/DefaultController.php
+++ src/WeatherBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace WeatherBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use WeatherBundle\Model\Location;
 use WeatherBundle\Utils\AccuWeatherProvider;
 use WeatherBundle\Utils\DelegatingWeatherProvider;
 use WeatherBundle\Utils\WeatherProviderInterface;
@@ -17,7 +18,8 @@ class DefaultController extends Controller
     public function indexAction(Request $request, WeatherProviderInterface $weatherProvider)
     {
         return $this->render('default/index.html.twig', [
-            'base_dir' => get_class($weatherProvider)
+            'class_name' => get_class($weatherProvider),
+            'weather' => $weatherProvider->fetch(new Location(55, 55))->getTemperature()
         ]);
     }
 }
